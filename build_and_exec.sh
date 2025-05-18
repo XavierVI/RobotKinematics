52242aec4dea74e6bc89797af232f71f8c08d73b
xavier <vigenesisvi@gmail.com>
2025-05-18 14:25:31 -0600

    started writing code for moving a planar rigid body

---
+++ build_and_exec.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ~/projects/RobotKinematics/build
+
+cmake --build .
+./modern_robotics
--- src/modern_robotics.cpp
+++ src/modern_robotics.cpp
@@ -1,6 +1,51 @@
+#include <iostream>
+#include <Eigen/Dense>
+
 /**
- * This class is basically code written to learn the kinematics
- * behind robot arms.
- *  */
+ * This file is used to write code to help solidify concepts from the
+ * modern robotics textbook.
+ **/
 
 
+
+class PlanarRigidBody {
+
+  private:
+    Eigen::Matrix2d space_frame;
+    Eigen::Matrix2d body_frame;
+
+    // orientation and position of the body frame wrt the space frame
+    Eigen::Matrix2d P;
+    Eigen::Vector2d p;
+    int i;
+
+  public:
+    PlanarRigidBody(Eigen::Matrix2d P, Eigen::Vector2d p) {
+      space_frame = Eigen::Matrix2d {
+        {1, 0},
+        {0, 1}
+      };
+
+      this->P = P;
+      this->p = p;
+    }
+};
+
+int main(int argc, char* argv[]) {
+  // creating an example of exponential coordinates
+  // space frame
+  Eigen::Matrix2d s_frame {
+    {1, 0},
+    {0, 1}
+  };
+
+  // orientation of {b} wrt {s}
+  Eigen::Matrix2d P {
+    {}
+  };
+
+  Eigen::Vector2d p {0, 0};
+
+  std::cout << s_frame << std::endl;
+  return 0;
+}
