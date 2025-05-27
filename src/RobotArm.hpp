0ee56c7222562e138ed7c059efc3b3da4c085b34
xavier <vigenesisvi@gmail.com>
2025-05-27 01:50:27 -0600

    added a method to compute the forward kinematics

--- src/RobotArm.hpp
+++ src/RobotArm.hpp
@@ -15,7 +15,41 @@ template <int J> class RobotArm {
     RobotArm(Eigen::Matrix4d M, Eigen::Matrix<double, J, 6> Slist) 
       : M(M), Slist(Slist) {}
 
-    void forwardSpaceKin() {}
+    
+    /**
+     * The forward kinematics in the space frame are computed using
+     * the terms exp(Slist[i] * angle[i]) for i = 0, ..., J-1.
+     * 
+     * This function returns a transformation matrix which represents
+     * the final pose of the arm.
+     */
+    Eigen::Matrix4d forwardSpaceKin(Eigen::Vector<double, J> angles) {
+      Eigen::Matrix4d T_sb = M;
+      Eigen::ArithmeticSequence seq = Eigen::seq(0, 2);
+
+      for (int i = J - 1; i >= 0; i--) {
+        // update the rotational component
+        Eigen::Matrix3d skew_symm {
+          {0, -Slist[i, 2], Slist[i, 1]},
+          {Slist[i, 2], 0, -Slist[i, 0]},
+          {-Slist[i, 1], Slist[i, 0], 0}
+        };
+
+        T_sb(seq, seq) = 
+            Eigen::Matrix3d::Identity() 
+              + std::cos(angles[i])*skew_symm 
+              + (1 - std::cos(angles[i]))*skew_symm*skew_symm;
+
+        // update the position
+        T_sb(seq, 3) = 
+            (Eigen::Matrix3d::Identity()
+              + (1 - std::cos(angles[i]))*skew_symm
+              + (angles[i] - std::sin(angles[i]))*skew_symm*skew_symm
+            )*Slist(2, 6);
+      }
+
+      return T_sb;
+    }
 
     void forwardBodyKin() {}
 
