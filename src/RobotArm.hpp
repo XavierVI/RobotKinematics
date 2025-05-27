41bbf623e26f604d7867a54072a9ce9ea7d6fed9
xavier <vigenesisvi@gmail.com>
2025-05-27 17:10:03 -0600

    updated the matrix exponential method

--- src/RobotArm.hpp
+++ src/RobotArm.hpp
@@ -14,14 +14,13 @@ template <int J> class RobotArm {
     /**
      * This method computes a matrix exponential using Slist and a
      * given angle. It's pretty much a helper function for the
-     * methods which compute forward kinematics (space frame).
+     * methods which compute forward kinematics.
      */
-    Eigen::Vector<double, 6> matrixExp6Space(int screw_axis_idx, double angle) {
+    Eigen::Vector<double, 6> matrixExp6(Eigen::Vector<double, 6> screw_axis, double angle) {
       Eigen::Matrix4d mat_exp = Eigen::Matrix4d::Identity();
-      Eigen::Vector6d S = Slist.row(screw_axis_idx);
       
-      Eigen::Vector3d omega = S.head<3>();
-      Eigen::Vector3d v = S.tail<3>();
+      Eigen::Vector3d omega = screw_axis.head<3>();
+      Eigen::Vector3d v = screw_axis.tail<3>();
 
       if (omega.norm() == 1) {
         // computing the skew-symmetric representation
@@ -56,56 +55,6 @@ template <int J> class RobotArm {
       return mat_exp;
     }
     
-
-    /**
-     * This method computes a matrix exponential using Slist and a
-     * given angle. It's pretty much a helper function for the
-     * methods which compute forward kinematics (body frame).
-     * 
-     * TODO: modify this method and matrixExp6Space to reduce
-     * duplicate code.
-     */
-    Eigen::Vector<double, 6> matrixExp6Body(int screw_axis_idx, double angle) {
-      Eigen::Matrix4d mat_exp = Eigen::Matrix4d::Identity();
-      Eigen::Vector6d S = Slist.row(screw_axis_idx);
-      Eigen::Vector6d B = M.inverse().adjoint() * S;
-      
-      Eigen::Vector3d omega = B.head<3>();
-      Eigen::Vector3d v = B.tail<3>();
-
-      if (omega.norm() == 1) {
-        // computing the skew-symmetric representation
-        // of omega
-        Eigen::Matrix3d skew_symm_mat {
-          { 0,       -omega(2),  omega(1) },
-          { omega(2), 0,         -omega(0)},
-          {-omega(1), omega(0),  0        }
-        };
-
-        Eigen::Matrix3d skew_symm_mat_sr = skew_symm_mat * skew_symm_mat;
-
-        // compute the orientation
-        mat_exp.topLeftCorner<3, 3>() = 
-            Eigen::Matrix3d::Identity() 
-              + std::sin(angle) * skew_symm_mat
-              + (1 - std::cos(angle)) * skew_symm_mat_sr;
-
-        // compute the position
-        mat_exp.block<3, 1>(0, 3) = 
-            (
-              Eigen::Matrix3d::Identity()
-              + (1 - std::cos(angles[i])) * skew_symm_mat
-              + (angles[i] - std::sin(angle)) * skew_symm_mat_sr
-            ) * v;
-      }
-
-      else if (omega.norm() == 0) {
-        mat_exp.block<3, 1>(0, 3) = v * angle;
-      }
-
-      return mat_exp;
-    }
-
 
   public:
     // TODO: might be worthwhile to add a check which verifies
@@ -125,7 +74,7 @@ template <int J> class RobotArm {
       Eigen::Matrix4d T_sb = M;
 
       for (int i = J - 1; i >= 0; i--) {
-        Eigen::Matrix4d mat_exp = matrixExp6Space(i, angles[i]);
+        Eigen::Matrix4d mat_exp = matrixExp6Space(Slist.row(i), angles[i]);
         T_sb = mat_exp * T_sb;
       }
 
@@ -136,7 +85,8 @@ template <int J> class RobotArm {
       Eigen::Matrix4d T_bb = M;
 
       for (int i = 0; i < J; i++) {
-        Eigen::Matrix4d mat_exp = matrixExp6Body(i, angles[i]);
+        Eigen::Vector<double, 6> B = M.inverse().adjoint() * Slist.row(i);
+        Eigen::Matrix4d mat_exp = matrixExp6Body(B, angles[i]);
         T_bb =  T_bb * mat_exp;
       }
 
